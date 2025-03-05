e552abe92998371ee2f9bc4abcffa04ebed774d3
Nisha R K <55803996+nishark90@users.noreply.github.com>
2025-03-05 21:07:02 +0530

    Add project files.

---
+++ ConsoleApp2 mini 4 num/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_mini_4_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 4 numbers :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            if (a > b && a > c && a > d)
+            { Console.WriteLine("The grater number is :" + a); }
+            else if (b > c && b > d)
+            { Console.WriteLine("The grater number is :" + b); }
+            else if (c > d)
+            { Console.WriteLine("The grater number is :" + c); }
+            else
+            { Console.WriteLine("The grater number is :" + d); }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
